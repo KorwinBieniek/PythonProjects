289fb7938de1864ad7aa40c4eb49adf5b7ca1029
Korwin <korwin-bieniek@wp.pl>
2022-08-31 04:50:50 +0200

    Simple AI chatbot using Deep Learning model to choose answers for whatever you ask him.

--- IntelligentAIChatbot/chatbot.py
+++ IntelligentAIChatbot/chatbot.py
@@ -11,8 +11,8 @@ from tensorflow.keras.models import load_model
 lemmatizer = WordNetLemmatizer()
 intents = json.loads(open('intents.json').read())
 
-words = pickle.load(open('words.pkl', 'rb'))
-classes = pickle.load(open('classes.pkl', 'rb'))
+words = pickle.load(open('../words.pkl', 'rb'))
+classes = pickle.load(open('../classes.pkl', 'rb'))
 model = load_model('chatbot_model.model')
 
 
--- IntelligentAIChatbot/training.py
+++ IntelligentAIChatbot/training.py
@@ -35,8 +35,8 @@ words = sorted(set(words))
 
 classes = sorted(set(classes))
 
-pickle.dump(words, open('words.pkl', 'wb'))
-pickle.dump(classes, open('classes.pkl', 'wb'))
+pickle.dump(words, open('../words.pkl', 'wb'))
+pickle.dump(classes, open('../classes.pkl', 'wb'))
 
 training = []
 output_empty = [0] * len(classes)
